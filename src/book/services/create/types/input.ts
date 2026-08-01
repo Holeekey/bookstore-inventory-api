4e862c4b1cf09d356b071f0a32e5b26a87a3a325
Holeekey <danbrunosv@gmail.com>
2026-08-01 09:37:28 -0400

    feat: :heavy_plus_sign: class validator added to project

---
+++ src/book/services/create/types/input.ts
@@ -0,0 +1,26 @@
+import { IsInt, IsNumber, IsString, Min } from 'class-validator'
+
+export class CreateBookInput {
+  @IsString()
+  title: string
+
+  @IsString()
+  author: string
+
+  @IsString()
+  isbn: string
+
+  @IsNumber()
+  @Min(0)
+  costUsd: number
+
+  @IsInt()
+  @Min(0)
+  stockQuantity: number
+
+  @IsString()
+  category: string
+
+  @IsString()
+  supplierCountry: string
+}
